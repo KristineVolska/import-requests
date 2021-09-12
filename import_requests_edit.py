75af89c996554b64309fc9a9b4ea4f8b2946ed0d
KristineVolska <46025401+KristineVolska@users.noreply.github.com>
2021-09-12 17:19:04 +0300

    Improvements

--- import_requests_edit.py
+++ import_requests_edit.py
@@ -1,12 +1,10 @@
 import requests
 import csv
-import unicodedata
 import getpass
 
 # Set the request parameters
 # Change the URL according to what information is desired.
 subdomain = input("Enter your Zendesk Subdomain (not full URL, but something such as your company name): ")
-#subdomain = 'soundid-reference'
 url = 'https://' + subdomain +'.zendesk.com/api/v2/help_center/en-us/articles.json?sort_by=title&sort_order=asc'
 
 # Use Your Zendesk Support Sign-On Credentials
@@ -14,10 +12,9 @@ user = input("Enter your the Email Address tied to your Zendesk Account: ")
 pwd = getpass.getpass("Enter your Zendesk Password: ")
 
 # Path of the outputted csv file
-csvfile = 'HelpCenterInformation.csv'
+csvfile = f'{subdomain}_articles.csv'
 
-# This loop cycles through all pages of articles, converts the unicode
-# to an integer, and writes the integers to an array
+# Comment out or remove the unnecessary attributes:
 attributes = {
         'id': 'Article ID', 
         'title': 'Article Title',
@@ -32,18 +29,24 @@ attributes = {
         }
 
 list_of_lists = []
+label_names_tuples_list = []
 
 for key in attributes.keys():
         list_of_lists.append([attributes[key]])
 
+# This loop cycles through all pages of articles
 while url:
         response = requests.get(url, auth = (user, pwd))
         data = response.json()
 
         for article in data['articles']:
+                label_names_tuples_list.append(tuple(article['label_names']))
                 list_id = 0
                 for key in attributes.keys():
-                        list_of_lists[list_id].append(str(article[key])) #unicodedata.normalize('NFKD', title)
+                        if key == 'label_names':
+                                list_of_lists[list_id].append('')
+                        else:
+                                list_of_lists[list_id].append(str(article[key]))
                         list_id += 1
         print(data['next_page'])
         url = data['next_page']
@@ -52,11 +55,15 @@ print("Number of articles:")
 print (len(list_of_lists[0]))
 
 # Data Transposition
-# nontransposed_data = [("Article ID","Article Title","URL","Vote Sum"), [output_1], [output_2], [output_3],[output_4]]
-# transposed_data = zip(*nontransposed_data)
+transposed_data = zip(*list_of_lists)
 
 # Write to a csv file
-with open(csvfile, 'w') as fp:
+with open(csvfile, 'w', newline='') as fp:
         writer = csv.writer(fp, dialect = 'excel')
-        for attr_list in list_of_lists:
-                writer.writerows([attr_list])
+        article_no = 0
+        for article_attr in transposed_data:
+                if article_no != 0:
+                        article_attr += label_names_tuples_list[article_no - 1]
+                writer.writerows([article_attr])
+                article_no += 1
+        
